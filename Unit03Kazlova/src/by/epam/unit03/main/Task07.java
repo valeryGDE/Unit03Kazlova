248178adc1b5261eaf41f8eb095f9d412397823b
valeryGDE <60448492+valeryGDE@users.noreply.github.com>
2021-05-26 10:23:59 +0300

    Update Task07.java

--- Unit03Kazlova/src/by/epam/unit03/main/Task07.java
+++ Unit03Kazlova/src/by/epam/unit03/main/Task07.java
@@ -5,53 +5,47 @@ public class Task07 {
 	public static void main(String[] args) {
 //		Написать программу, переводящую римские цифры в арабские.
 		
-		String roman;
+		String roman = "IVXLCDM";
 		int arabic;
-		
-		for(int i = 1; i <=10; i++) {
-			arabic = i;
+		char romanChar;
 
-		switch (arabic) {
+		char[] romanArray = roman.toCharArray();
 
-		case 1:
-			roman = "I";
-			break;
-		case 2:
-			roman = "II";
-			break;
-		case 3:
-			roman = "III";
-			break;
-		case 4:
-			roman = "IV";
-			break;
-		case 5:
-			roman = "V";
-			break;
-		case 6:
-			roman = "VI";
-			break;
-		case 7:
-			roman = "VII";
-			break;
-		case 8:
-			roman = "VIII";
-			break;
-		case 9:
-			roman = "IX";
-			break;
-		case 10:
-			roman = "X";
-			break;
+		for (int i = 0; i < roman.length(); i++) {
+			romanChar = romanArray[i];
 
-		default:
-			System.out.println("Что-то не то с программой.");
-			return;
-			
+			switch (romanChar) {
+
+			case 'I':
+				arabic = 1;
+				break;
+			case 'V':
+				arabic = 5;
+				break;
+			case 'X':
+				arabic = 10;
+				break;
+			case 'L':
+				arabic = 50;
+				break;
+			case 'C':
+				arabic = 100;
+				break;
+			case 'D':
+				arabic = 500;
+				break;
+			case 'M':
+				arabic = 1000;
+				break;
+
+			default:
+				System.out.println("Что-то не то с программой.");
+				return;
+
 			}
-		
-		System.out.println(arabic + " = " + roman);
-		
+
+			System.out.println(romanChar + " = " + arabic);
+
 		}
 
 	}
